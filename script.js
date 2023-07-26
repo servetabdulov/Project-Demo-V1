672c3d69b2c7628927f52e81c876e7a47b7e79f4
servetabdulov <servet.abdulov.2015@gmail.com>
2023-07-26 07:19:59 +0400

    add

--- script.js
+++ script.js
@@ -7,12 +7,10 @@ const nextBtn = document.querySelector(".next-btn");
 let counter = 0;
 let slideWidth = 0;
 
-// Slider geçişini sağlayan fonksiyon
 function slide() {
   slider.style.transform = `translateX(${-slideWidth * counter}px)`;
 }
 
-// Sonraki slayta geçmek için olay dinleyicisi
 nextBtn.addEventListener("click", () => {
   counter++;
   if (counter >= slides.length) {
@@ -21,7 +19,6 @@ nextBtn.addEventListener("click", () => {
   slide();
 });
 
-// Önceki slayta geçmek için olay dinleyicisi
 prevBtn.addEventListener("click", () => {
   counter--;
   if (counter < 0) {
@@ -30,18 +27,16 @@ prevBtn.addEventListener("click", () => {
   slide();
 });
 
-// Sayfa yüklenirken ve tarayıcı boyutu değiştiğinde sliderı yeniden düzenle
 window.addEventListener("resize", () => {
   slideWidth = sliderContainer.clientWidth / 5;
   slide();
 });
 
-// İlk yüklenmede ve tarayıcı boyutu değiştiğinde sliderı düzenle
 window.addEventListener("load", () => {
   slideWidth = sliderContainer.clientWidth / 5;
   slide();
 });
-// IKINCI SLIDER
+
 const sliderContainer2 = document.querySelector(".slider-container2");
 const slider2 = document.querySelector(".slider2");
 const slides2 = document.querySelectorAll(".slide2");
@@ -51,12 +46,10 @@ const nextBtn2 = document.querySelector(".next-btn2");
 let counter2 = 0;
 let slideWidth2 = 0;
 
-// Slider geçişini sağlayan fonksiyon
 function slide2() {
   slider2.style.transform = `translateX(${-slideWidth2 * counter2}px)`;
 }
 
-// Sonraki slayta geçmek için olay dinleyicisi
 nextBtn2.addEventListener("click", () => {
   counter2++;
   if (counter2 >= slides2.length) {
@@ -65,7 +58,6 @@ nextBtn2.addEventListener("click", () => {
   slide2();
 });
 
-// Önceki slayta geçmek için olay dinleyicisi
 prevBtn2.addEventListener("click", () => {
   counter2--;
   if (counter2 < 0) {
@@ -74,13 +66,11 @@ prevBtn2.addEventListener("click", () => {
   slide2();
 });
 
-// Sayfa yüklenirken ve tarayıcı boyutu değiştiğinde sliderı yeniden düzenle
 window.addEventListener("resize", () => {
   slideWidth2 = sliderContainer2.clientWidth / 5;
   slide2();
 });
 
-// İlk yüklenmede ve tarayıcı boyutu değiştiğinde sliderı düzenle
 window.addEventListener("load", () => {
   slideWidth2 = sliderContainer2.clientWidth / 5;
   slide2();
@@ -88,13 +78,10 @@ window.addEventListener("load", () => {
 
 const burgerButton = document.querySelector(".burger-button");
 
-// Menüyü seçin
 const menu = document.querySelector(".menu");
 
-// Burger butonuna tıklanma olayını dinleyin
 burgerButton.addEventListener("click", toggleMenu);
 
-// Menüyü açma ve kapama işlemini yönetin
 function toggleMenu() {
   menu.classList.toggle("active");
 }
